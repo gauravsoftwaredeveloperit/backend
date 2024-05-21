cc9a1df634b794afd548d05eb179327b96a21d2a
gauravsoftwaredeveloperit <gauravkumarsingh2025@gmail.com>
2024-05-21 17:25:40 +0530

    Add comment,like,playlist,tweet models

---
+++ src/models/like.model.js
@@ -0,0 +1,32 @@
+import mongoose, {Schema} from "mongoose";
+
+
+
+const likeSchema = new Schema(
+    {
+        
+        video:{
+            type: Schema.Types.ObjectId,
+            ref: "Video"
+
+        },
+        comment:{
+            type: Schema.Types.ObjectId,
+           ref: "comment"
+        },
+        tweet:{
+            type: Schema.Types.ObjectId,
+           ref: "Tweet"
+        },
+        likedBy:{
+            type: Schema.Types.ObjectId,
+           ref: "User"
+        }
+       
+    },
+    {
+        timestamps:true
+    }
+)
+
+export const Like = mongoose.model("Like", likeSchema)
--- src/models/vedio.model.js
+++ src/models/vedio.model.js
@@ -1,4 +1,4 @@
-import mongoose, {Schema, Types} from "mongoose";
+import mongoose, {Schema,} from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const vedioSchema = new Schema (
